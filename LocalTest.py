7643bdbbcb30d5f96ee99501374a16d2147fbc7a
Jun Jiang <Jun.Jiang@harman.com>
2020-02-06 15:08:22 +0800

    Modify compare results' excel outputs; Modify local test cases.

--- LocalTest.py
+++ LocalTest.py
@@ -3,51 +3,106 @@ import numpy as np
 from matplotlib import pyplot as plt
 from skimage.measure import compare_ssim
 
-img = cv2.imread('gray/ac_button_alternativemode03_normal.png', cv2.IMREAD_UNCHANGED)
+img1 = cv2.imread('future-skin-red/res/mipmap-mdpi/menu_music_background_press.png', cv2.IMREAD_UNCHANGED)
+img2 = cv2.imread('launcher/launcher/mipmap-mdpi/menu_music_background_press.png', cv2.IMREAD_UNCHANGED)
+img3 = cv2.imread('launcher/launcher/mipmap-mdpi/menu_music_background_select.png', cv2.IMREAD_UNCHANGED)
+print('multichannel')
+score2 = compare_ssim(img1, img2, multichannel=True)
+score3 = compare_ssim(img1, img3, multichannel=True)
+print('score2={}, score3={}'.format(score2, score3))
 
-ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
-ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
-ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
-ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
-alphaImg = img[:,:,3]
-rgbImg = img[:,:,:3]
+#to alpha
+alpha1 = img1[:,:,3]
+alpha2 = img2[:,:,3]
+alpha3 = img3[:,:,3]
+print('alpha')
+alphaScore2 = compare_ssim(alpha1, alpha2, multichannel=True)
+alphaScore3 = compare_ssim(alpha1, alpha3, multichannel=True)
+print('score2={}, score3={}'.format(alphaScore2, alphaScore3))
+#to bgr
+bgr1 = img1[:,:,:3]
+bgr2 = img2[:,:,:3]
+bgr3 = img3[:,:,:3]
+print('bgr')
+bgrScore2 = compare_ssim(bgr1, bgr2, multichannel=True)
+bgrScore3 = compare_ssim(bgr1, bgr3, multichannel=True)
+print('score2={}, score3={}'.format(bgrScore2, bgrScore3))
+#to gray
+gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+gray3 = cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY)
+print('gray')
+grayScore2 = compare_ssim(gray1, gray2, multichannel=True)
+grayScore3 = compare_ssim(gray1, gray3, multichannel=True)
+print('score2={}, score3={}'.format(grayScore2, grayScore3))
+#threshold
+(thresh, im_bw1) = cv2.threshold(img1, 1, 255, cv2.THRESH_BINARY)
+(thresh, im_bw2) = cv2.threshold(img2, 1, 255, cv2.THRESH_BINARY)
+(thresh, im_bw3) = cv2.threshold(img3, 1, 255, cv2.THRESH_BINARY)
+print('threshold')
+thsScore2 = compare_ssim(im_bw1, im_bw2, multichannel=True)
+thsScore3 = compare_ssim(im_bw1, im_bw3, multichannel=True)
+print('score2={}, score3={}'.format(thsScore2, thsScore3))
 
-img2 = cv2.imread('blue/ac_button_alternativemode03_press.png', cv2.IMREAD_UNCHANGED)
+plt.subplot(5, 3, 1)
+plt.imshow(img1, 'gray')
+plt.title('img1')
 
-ret,thresh21 = cv2.threshold(img2,127,255,cv2.THRESH_BINARY)
-ret,thresh22 = cv2.threshold(img2,127,255,cv2.THRESH_BINARY_INV)
-ret,thresh23 = cv2.threshold(img2,127,255,cv2.THRESH_TRUNC)
-ret,thresh24 = cv2.threshold(img2,127,255,cv2.THRESH_TOZERO)
-ret,thresh25 = cv2.threshold(img2,127,255,cv2.THRESH_TOZERO_INV)
-alphaImg2 = img2[:,:,3]
-rgbImg2 = img2[:,:,:3]
+plt.subplot(5, 3, 2)
+plt.imshow(img2, 'gray')
+plt.title('img2={:f}'.format(score2))
 
-titles = ['Original Image{:d}','BINARY{:d}','BINARY_INV{:d}','TRUNC{:d}','TOZERO{:d}','TOZERO_INV{:d}', 'ALPHA{:d}', 'RGB{:d}']
-images = [img, thresh1, thresh2, thresh3, thresh4, thresh5, alphaImg, rgbImg]
-images2 = [img2, thresh21, thresh22, thresh23, thresh24, thresh25, alphaImg2, rgbImg2]
+plt.subplot(5, 3, 3)
+plt.imshow(img3, 'gray')
+plt.title('img3={:f}'.format(score3))
 
-plt.subplot(1, 2, 1),plt.hist(img.ravel(),256,[0,256])
-plt.subplot(1, 2, 2),plt.hist(img2.ravel(),256,[0,256])
-plt.show()
+plt.subplot(5, 3, 4)
+plt.imshow(alpha1, 'gray')
+plt.title('alpha1')
 
-for i, j in zip(range(0, 24, 3), range(0, 8)):
-    plt.subplot(8,3,i+1),plt.imshow(images[j])
-    plt.title(titles[j].format(i + 1))
-    plt.xticks([]),plt.yticks([])
+plt.subplot(5, 3, 5)
+plt.imshow(alpha2, 'gray')
+plt.title('alpha2={:f}'.format(alphaScore2))
 
-    plt.subplot(8,3,i+2),plt.imshow(images2[j])
-    plt.title(titles[j].format(i + 2))
-    plt.xticks([]),plt.yticks([])
+plt.subplot(5, 3, 6)
+plt.imshow(alpha3, 'gray')
+plt.title('alpha3={:f}'.format(alphaScore3))
 
-    bgrScore, diff = compare_ssim(images[j], images2[j], multichannel=True, full=True)
-    diff = (diff * 255).astype("uint8")
-    plt.subplot(8,3,i+3),plt.imshow(diff)
-    plt.title('ssim={}'.format(bgrScore))
-    plt.xticks([]),plt.yticks([])
+plt.subplot(5, 3, 7)
+plt.imshow(gray1, 'gray')
+plt.title('gray1')
+
+plt.subplot(5, 3, 8)
+plt.imshow(gray2, 'gray')
+plt.title('gray2={:f}'.format(grayScore2))
+
+plt.subplot(5, 3, 9)
+plt.imshow(gray3, 'gray')
+plt.title('gray3={:f}'.format(grayScore3))
+
+plt.subplot(5, 3, 10)
+plt.imshow(im_bw1, 'gray')
+plt.title('ths1')
+
+plt.subplot(5, 3, 11)
+plt.imshow(im_bw2, 'gray')
+plt.title('ths2={:f}'.format(thsScore2))
+
+plt.subplot(5, 3, 12)
+plt.imshow(im_bw3, 'gray')
+plt.title('ths3={:f}'.format(thsScore3))
+
+plt.subplot(5, 3, 13)
+plt.imshow(bgr1, 'gray')
+plt.title('bgr1')
+
+plt.subplot(5, 3, 14)
+plt.imshow(bgr2, 'gray')
+plt.title('bgr2={:f}'.format(bgrScore2))
+
+plt.subplot(5, 3, 15)
+plt.imshow(bgr3, 'gray')
+plt.title('bgr3={:f}'.format(bgrScore3))
 
 plt.subplots_adjust(hspace=1, wspace=0.1)
 plt.show()
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
--- PyImgCmp.py
+++ PyImgCmp.py
@@ -1,11 +1,17 @@
 import os
 import cv2
+import sys
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
 
 from skimage.measure import compare_ssim
 
 from openpyxl import Workbook
 from openpyxl import load_workbook
 from openpyxl.worksheet.worksheet import Worksheet
+
+from openpyxl.styles import Color, PatternFill
 
 class FileCompareResult(object):
     CODE_ERROR = -1
@@ -16,23 +22,29 @@ class FileCompareResult(object):
         self.code = code
 
 class ImageCompareResult(FileCompareResult):
-    def __init__(self, imagefile, otherfile, diff, dimensions, code = FileCompareResult.CODE_SUCCESS):
+    def __init__(self, imagefile, otherfile, diff, dimensions, code = FileCompareResult.CODE_SUCCESS, reason = ''):
         super().__init__(code)
         self.imagefile = imagefile
         self.otherfile = otherfile
         self.dimensions = dimensions
         self.diff = diff
+        self.reason = reason
+
+    def __eq__(self, other) -> bool:
+        return self.imagefile == other.imagefile and self.otherfile == other.otherfile
 
     def __str__(self):
-        '''
-        if FileCompareResult.CODE_ERROR == self.code:
-            return '{},w:{:d},h:{:d},c:{:d}'.format(self.imagefile, self.dimensions[0], self.dimensions[1], self.dimensions[2])
-        else:
-        '''
-        return '{},{},{:.10f},w:{:d},h:{:d},c:{:d}'.format(self.imagefile, self.otherfile, self.diff, self.dimensions[0], self.dimensions[1], self.dimensions[2])
+        return '{},{},{:.10f},w:{:d},h:{:d},c:{:d}, {}'.format(self.imagefile, self.otherfile, self.diff, self.dimensions[0], self.dimensions[1], self.dimensions[2], self.reason)
 
     def iterator(self):
         return str(self).split(',')
+
+    def correct(self) -> bool:
+        split = self.imagefile.split('\\')
+        imagefile_name = split[len(split) - 1]
+        split = self.otherfile.split('\\')
+        otherfile_name = split[len(split) - 1]
+        return imagefile_name == otherfile_name
 
 class ImageComparater(object):
     IGNORE_DIFF_SCORE = -1
@@ -46,15 +58,15 @@ class ImageSSIMComparater(ImageComparater):
         img1 = cv2.imread(imagefile1, cv2.IMREAD_UNCHANGED)
         img2 = cv2.imread(imagefile2, cv2.IMREAD_UNCHANGED)
         if not img1.dtype == img2.dtype or not img1.shape == img2.shape:
-            #print ('Invalid compare inputs: left={}{}, right={}{}'.format(img1.dtype, img1.shape, img2.dtype, img2.shape))
-            return ImageCompareResult(imagefile1, imagefile2, ImageComparater.IGNORE_DIFF_SCORE, img1.shape, FileCompareResult.CODE_WARN)
-        
+            return ImageCompareResult(imagefile1, imagefile2, ImageComparater.IGNORE_DIFF_SCORE, img1.shape, FileCompareResult.CODE_WARN, 'dimensions not match')
+        shape1 = img1.shape
+        shape2 = img2.shape
         try:
             bgrScore = compare_ssim(img1, img2, multichannel=True)
-        except ValueError:
-            print ('Invalid compare inputs: left={}{}{}, right={}{}{}'.format(imagefile1, img1.dtype, img1.shape, imagefile2, img2.dtype, img2.shape))
-            return ImageCompareResult(imagefile1, imagefile2, ImageComparater.IGNORE_DIFF_SCORE, img1.shape, FileCompareResult.CODE_ERROR)
-        return ImageCompareResult(imagefile1, imagefile2, 1.0 - bgrScore, img1.shape)
+        except ValueError as e:
+            print ('Invalid compare inputs: left={}{}{}, right={}{}{}'.format(imagefile1, img1.dtype, shape1, imagefile2, img2.dtype, shape2))
+            return ImageCompareResult(imagefile1, imagefile2, ImageComparater.IGNORE_DIFF_SCORE, shape1, FileCompareResult.CODE_ERROR, str(e))
+        return ImageCompareResult(imagefile1, imagefile2, 1.0 - bgrScore, shape1)
 
 class ImageFileIterator(object):
     def __init__(self, folder):
@@ -89,20 +101,21 @@ class ImageFileIterator(object):
 class ImageFileCompareTask(ImageFileIterator):
     MAX_DIFF_RECORD = 10
 
-    def __init__(self, imagefile, folder, comparater: ImageComparater):
+    def __init__(self, imagefile, folder):
         super().__init__(folder)
         self.imagefile = imagefile
-        self.comparater = comparater
+        self.comparater = ImageSSIMComparater()
         self.equal_list = []
         self.similar_list = []
+        self.warn_list = []
         self.error_list = []
 
     def insert_result(self, result: ImageCompareResult):
         if FileCompareResult.CODE_ERROR == result.code:
             self.error_list.append(result)
         elif FileCompareResult.CODE_WARN == result.code:
-            #dimensions not equal, pass over
-            return
+            if len(self.warn_list) == 0:
+                self.warn_list.append(result)
         elif FileCompareResult.CODE_SUCCESS == result.code:
             if ImageComparater.EQUAL_DIFF_SCORE == result.diff:
                 self.equal_list.append(result)
@@ -128,24 +141,43 @@ class ImageFileCompareTask(ImageFileIterator):
                 self.similar_list[position] = result
 
     def process(self, imagefile):
-        self.insert_result(self.comparater.calculate_diff(self.imagefile, imagefile))
+        result = self.comparater.calculate_diff(self.imagefile, imagefile)
+        self.insert_result(result)
 
     def start(self):
         super().start()
-        if not len(self.equal_list) == 0:
+        len_equal = len(self.equal_list)
+        len_similar = len(self.similar_list)
+        len_error = len(self.error_list)
+        len_warn = len(self.warn_list)
+        #print('{}: equal {:d}, similar {:d}, error {:d}'.format(self.imagefile, len_equal, len_similar, len_error))
+        if not len_equal == 0:
             #we only need the equal one
             self.similar_list.clear
             self.error_list.clear
-        elif not len(self.similar_list) == 0:
-            if len(self.similar_list) == 1:
+        elif not len_similar == 0:
+            if len_similar == 1:
                 #the only one similar, can be treated as equal
                 self.equal_list.append(self.similar_list[0])
                 self.similar_list.clear
-        elif not len(self.error_list) == 0:
-            if len(self.error_list) == 1:
+        elif not len_error == 0:
+            if len_error == 1:
                 #the only one that dimensions match, can be treated as equal
                 self.equal_list.append(self.error_list[0])
                 self.error_list.clear
+
+        len_equal = len(self.equal_list)
+        len_similar = len(self.similar_list)
+        len_error = len(self.error_list)
+        #no record
+        if len_equal == 0 and len_similar == 0 and len_error == 0:
+            if len_warn > 0:
+                self.error_list.append(self.warn_list[0])
+        #maintain one record for similar and error list
+        if len_similar > 1:
+            del self.similar_list[1:len(self.similar_list)]
+        if len_error > 1:
+            del self.error_list[1:len(self.error_list)]
 
 class ResultPrinter(object):
     def print(self, compare_task: ImageFileCompareTask):
@@ -162,7 +194,6 @@ class ExcelPrinter(ResultPrinter):
                 if filename == file:
                     return load_workbook(filename)
         wbk = Workbook()
-
         wbk.save(filename)
         return load_workbook(filename)
 
@@ -173,6 +204,8 @@ class ExcelPrinter(ResultPrinter):
         ws.title = default_sheet_name
         self.xlsxfile.save(filename)
         self.sheet_dict = dict()
+        self.sheet_dict[default_sheet_name] = 0
+        self.fill = PatternFill(patternType='solid', fill_type='solid', fgColor=Color('C4C4C4'))
 
     def finish_print(self):
         for ws in self.xlsxfile.worksheets:
@@ -192,10 +225,12 @@ class ExcelPrinter(ResultPrinter):
         ws = self.xlsxfile[sheet_name]
         for result in results_list:
             ws.append(result.iterator())
-        '''
-        #add empty row
-        ws.append([])
-        '''
+            if not result.correct():
+                self.sheet_dict[sheet_name] = self.sheet_dict.get(sheet_name, 0) + 1
+                for row in ws.iter_rows(min_row=ws.max_row):
+                    for cell in row:
+                        cell.fill = self.fill
+                ws.cell(row = 1, column = 8, value = int(self.sheet_dict[sheet_name]))
 
     def print(self, compare_task: ImageFileCompareTask):
         equal_list = compare_task.equal_list
@@ -206,24 +241,29 @@ class ExcelPrinter(ResultPrinter):
         len_error = len(error_list)
         if not len_equal == 0:
             #print equal sheet
+            #print('{} equal record {}'.format(compare_task.imagefile, len_equal))
             self.print_sheet(ExcelPrinter.EQUAL_SHEET_NAME, equal_list)
         elif not len_similar == 0:
             #print similar sheet
+            #print('{} similar record {}'.format(compare_task.imagefile, len_similar))
             self.print_sheet(ExcelPrinter.SIMILAR_SHEET_NAME, similar_list)
         elif not len_error == 0:
             #print error sheet
+            #print('{} error record {}'.format(compare_task.imagefile, len_error))
             self.print_sheet(ExcelPrinter.ERROR_SHEET_NAME, error_list)
+        else:
+            #no result
+            print('{} no record !!!'.format(compare_task.imagefile))
         self.finish_print()
 
 class ImageFolderCompareTask(ImageFileIterator):
-    def __init__(self, folder1, folder2, comparater: ImageComparater, printer: ResultPrinter):
+    def __init__(self, folder1, folder2, printer: ResultPrinter):
         super().__init__(folder1)
         self.otherfolder = folder2
-        self.comparater = comparater
         self.printer = printer
 
     def process(self, file):
-        file_compare_task = ImageFileCompareTask(file, self.otherfolder, self.comparater)
+        file_compare_task = ImageFileCompareTask(file, self.otherfolder)
         file_compare_task.start()
         # print results
         self.printer.print(file_compare_task)
@@ -233,7 +273,7 @@ if __name__ == '__main__':
     proj_folder = 'future-skin-red'
     xlsx_filename = 'launcher.xlsx'
 
-    comparater = ImageFolderCompareTask(proj_folder, ui_folder, ImageSSIMComparater(), ExcelPrinter(xlsx_filename))
+    comparater = ImageFolderCompareTask(proj_folder, ui_folder, ExcelPrinter(xlsx_filename))
     comparater.start()
 
 
